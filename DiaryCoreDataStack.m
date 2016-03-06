987593eec166be02b38665533a26658f4f50f2b7
nathanwty <mrcomrade@163.com>
2016-03-07 00:26:13 +0800

    图标

---
+++ DiaryCoreDataStack.m
@@ -0,0 +1,13 @@
+//
+//  DiaryCoreDataStack.m
+//  TheDays
+//
+//  Created by nathan on 16/3/6.
+//  Copyright © 2016年 nathanwty. All rights reserved.
+//
+
+#import "DiaryCoreDataStack.h"
+
+@implementation DiaryCoreDataStack
+
+@end
